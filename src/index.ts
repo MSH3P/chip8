79152453c0b28c57e44294e00ef15415b790c7f7
Grae <greypaper@live.ca>
2021-09-17 20:39:08 -0400

    Isolated the terminal plane from macintosh2.glb, can now be referenced and assigned a different material now. Will be used to draw a dynamic screen.

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,96 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-const renderer = new THREE.WebGLRenderer();
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+const essentials = () => {
+  return {
+    scene: new THREE.Scene(),
+    camera: new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    ),
+    renderer: new THREE.WebGLRenderer(),
+  };
+};
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+const appendToDom = (renderer: any) => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+};
 
-document.body.appendChild(renderer.domElement);
+const addLight = (scene: any) => {
+  const light = new THREE.AmbientLight(0x404040); // soft white light
+  scene.add(light);
+};
+
+const cameraPosition = (camera: any) => {
+  camera.position.z = 5;
+};
+
+const image = (width: number, height: number) => {
+  const canvas = document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = height;
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(0, 0, width, height);
+
+  return canvas.toDataURL();
+};
+
+const updateTerminalMesh = (width, height) => {
+  const img = image(width, height);
+  return new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load(img),
+    side: THREE.DoubleSide,
+  });
+};
+
+const initControls = (isDev, camera, renderer) => {
+  return isDev ? new OrbitControls(camera, renderer.domElement) : null;
+};
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  if (computer != null) computer.rotation.y += 0.005;
+  if (controls != null) controls.update();
+}
+
+const init = essentials();
+
+const scene = init.scene;
+const camera = init.camera;
+const renderer = init.renderer;
+let controls = initControls(false, camera, renderer);
+
+appendToDom(renderer);
+cameraPosition(camera);
+addLight(scene);
+
+let computer;
+let terminal;
 
 const loader = new GLTFLoader();
-const light = new THREE.AmbientLight(0x404040); // soft white light
-scene.add(light);
-
-let arcade;
-
 loader.load(
   'assets/models/macintosh2.glb',
   function (gltf) {
-    arcade = gltf.scene;
-    scene.add(arcade);
+    computer = gltf.scene;
+    terminal = computer.children[3];
+
+    terminal.material = new THREE.MeshBasicMaterial({
+      map: new THREE.TextureLoader().load(image(100, 100)),
+      side: THREE.DoubleSide,
+    });
+
+    computer.rotation.y = 270 * (Math.PI / 180);
+    scene.add(computer);
   },
   undefined,
   function (error) {
     console.error(error);
   }
 );
-camera.position.z = 2;
 
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-
-  arcade.rotation.y += 0.005;
-}
 animate();
