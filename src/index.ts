f676e6d4084a6c71a60e36260882061d7523109b
Grae <greypaper@live.ca>
2021-09-18 06:04:42 -0400

    Added Directional light instead, also working on drawing a screen to terminal mesh.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { Texture, Vector3 } from 'three';
 const essentials = () => {
   return {
     scene: new THREE.Scene(),
@@ -20,8 +21,9 @@ const appendToDom = (renderer: any) => {
 };
 
 const addLight = (scene: any) => {
-  const light = new THREE.AmbientLight(0x404040); // soft white light
-  scene.add(light);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.25);
+  directionalLight.position.add(new Vector3(2, 3, 2));
+  scene.add(directionalLight);
 };
 
 const cameraPosition = (camera: any) => {
@@ -33,18 +35,14 @@ const image = (width: number, height: number) => {
   canvas.width = width;
   canvas.height = height;
   const ctx = canvas.getContext('2d');
-  ctx.fillStyle = 'blue';
+
+  ctx.fillStyle = 'red';
   ctx.fillRect(0, 0, width, height);
 
-  return canvas.toDataURL();
-};
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(200, 150, 10, 10);
 
-const updateTerminalMesh = (width, height) => {
-  const img = image(width, height);
-  return new THREE.MeshBasicMaterial({
-    map: new THREE.TextureLoader().load(img),
-    side: THREE.DoubleSide,
-  });
+  return canvas.toDataURL();
 };
 
 const initControls = (isDev, camera, renderer) => {
@@ -63,7 +61,8 @@ const init = essentials();
 const scene = init.scene;
 const camera = init.camera;
 const renderer = init.renderer;
-let controls = initControls(false, camera, renderer);
+
+let controls = initControls(true, camera, renderer);
 
 appendToDom(renderer);
 cameraPosition(camera);
@@ -79,9 +78,12 @@ loader.load(
     computer = gltf.scene;
     terminal = computer.children[3];
 
+    const texture = new THREE.TextureLoader().load(image(640, 320), () => {
+      texture.needsUpdate = true;
+    });
+
     terminal.material = new THREE.MeshBasicMaterial({
-      map: new THREE.TextureLoader().load(image(100, 100)),
-      side: THREE.DoubleSide,
+      map: texture,
     });
 
     computer.rotation.y = 270 * (Math.PI / 180);
